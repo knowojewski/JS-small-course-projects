89c2e15a477f02e2d2e4d85e92334548f597a745
knowojewski <xsardas.geza@gmail.com>
2020-05-19 10:43:13 +0200

    GitHub Finder finished app

--- githubFinder/github.js
+++ githubFinder/github.js
@@ -2,15 +2,21 @@ class GitHub {
     constructor() {
         this.client_id = '9d99a3eb959670afad04';
         this.client_secret = 'b526f53de343c168c8ce28c62367b1fbde0190e7';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
