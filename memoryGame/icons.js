4ccfc29d643b649e8111b086e3951d8ae0c47b2b
knowojewski <xsardas.geza@gmail.com>
2020-06-27 20:37:09 +0200

    MemoryGame project

---
+++ memoryGame/icons.js
@@ -0,0 +1,31 @@
+const icons = [
+    {
+        icon: 'A',
+        number: 0
+    },
+
+    {
+        icon: 'B',
+        number: 1
+    },
+
+    {
+        icon: 'C',
+        number: 2
+    },
+
+    {
+        icon: 'D',
+        number: 3
+    },
+
+    {
+        icon: 'E',
+        number: 4
+    },
+
+    {
+        icon: 'F',
+        number: 5
+    }
+]
---
+++ memoryGame/main.js
@@ -0,0 +1,121 @@
+const board = document.querySelector('.game-board');
+const play = document.querySelector('.play');
+const score = document.querySelector('.score');
+
+let cardArr = [];
+let result = 0;
+
+
+const loadEventListeners = () => {
+    play.addEventListener('click', loadBoard)
+};
+
+
+
+// -------- Loading Board ------------
+
+
+function loadBoard() {
+    play.innerText = 'TRY AGAIN';
+    board.innerHTML = '';
+    result = 0;
+    score.innerHTML = result;
+    
+    for(let i = 0; i < 12; i++) {
+        const cardContainer = document.createElement('div');
+        const card = document.createElement('div');
+        const front = document.createElement('figure');
+        const back = document.createElement('figure');
+        
+        cardContainer.className = 'card-container';
+        card.className = 'card';
+        front.className = 'front';
+        back.className = 'back';
+
+        front.innerHTML = '<i class="fas fa-hand-pointer"></i>';
+
+        card.appendChild(front);
+        card.appendChild(back);
+        cardContainer.appendChild(card);
+
+        cardContainer.addEventListener('click', rotateBlock);
+
+        board.appendChild(cardContainer);
+    }
+
+    const allBacks = document.querySelectorAll('.back');
+    let num = 0;
+
+    for(let j = 0; j < 12; j += 2) {
+        allBacks[j].setAttribute('id', num);
+        allBacks[j + 1].setAttribute('id', num);
+        
+        const icon = icons[num].icon
+        allBacks[j].innerHTML = icon;
+        allBacks[j + 1].innerHTML = icon;
+        num++;
+    }
+
+    const allCards = document.querySelectorAll('.card-container');
+
+    allCards.forEach(item => {
+        item.style.order = Math.floor(Math.random()*100);
+    });
+
+};
+
+
+
+// --------- Rotating Block ------------
+
+
+function rotateBlock(event) {
+    this.classList.add('rotate');
+    
+    cardArr.push(this);
+    if(cardArr.length === 2) {
+        checkIfMatches(cardArr);
+    } else if(cardArr.length > 2) {
+        console.log('More than 2')
+    }
+
+};
+
+
+// --------- Check if Cards Matches ---------
+
+
+const checkIfMatches = cards => {
+    const firstFigure = cards[0].children[0].children[1];
+    const secondFigure = cards[1].children[0].children[1];
+    const allCards = document.querySelectorAll('.card-container');
+
+    console.log(allCards[0]);
+
+    if (firstFigure.innerHTML === secondFigure.innerHTML) {
+        result++;
+        score.innerText = result;
+
+        cardArr.forEach( item => {
+            console.log(item);
+            item.removeEventListener('click', rotateBlock);
+            item.style.cursor = 'auto';
+        });
+
+        cardArr = [];
+    } else {
+        allCards.forEach(item => item.removeEventListener('click', rotateBlock));
+
+        setTimeout(() => {
+            cards.forEach(item => item.classList.remove('rotate') );
+            allCards.forEach(item => item.addEventListener('click', rotateBlock));
+        }, 1000);
+        
+        cardArr = [];
+    }
+};
+
+
+
+
+document.addEventListener('DOMContentLoaded', loadEventListeners);
